1e33bdc7ac37a0534d4c9e7d639a74c2362a3f15
ViniciusTn <vinicius_t_nunes@estudante.sesisenai.org.br>
2025-09-02 12:20:38 -0300

    Cadastro

---
+++ cadastro.php
@@ -0,0 +1,24 @@
+<?php
+
+include("conexao.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+
+    $sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
+    $res = mysqli_query($conn, $sql);
+    if ($res) {
+        echo "Usuário cadastrado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar!";
+    }
+}
+
+?>
+
+<form method="POST">
+    Nome: <input type="text" name="nome"><br>
+    Email: <input type="email" name="email"><br>
+    <input type="submit" value="Cadastrar">
+</form>
